4b18f2c590915d4fa2c3ddfcf81542216ba1d4eb
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-07 14:02:31 -0700

    Testing with Android.

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,15 @@ import 'app/app.dart';
 void main() {
   runApp(const LeosMusicStoreApp());
   String writeKey;
-  if(Platform.isAndroid){
-    writeKey = "ANDROID_WRITE_KEY";
-  } else{ //iOS
-    writeKey = "IOS_WRITE_KEY";
+  if (Platform.isAndroid) {
+    writeKey = "Mh4yLhMbKmsvfuqo0HvsJQmpUdWo4B7L";
+  } else {
+    writeKey = "qvLBJg7UvuzfWKPmPOttAbLsOHm4y5AA";
   }
 
   Segment.config(
     options: SegmentConfig(
-      writeKey: 'qvLBJg7UvuzfWKPmPOttAbLsOHm4y5AA',
+      writeKey: writeKey,
       trackApplicationLifecycleEvents: false,
       amplitudeIntegrationEnabled: false,
       debug: false,
