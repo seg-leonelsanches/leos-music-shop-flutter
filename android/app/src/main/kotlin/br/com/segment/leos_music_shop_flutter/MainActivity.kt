b35d179385c01ff00c2c8d9240b48558252e40ac
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-07 13:43:52 -0700

    initial commit

---
+++ android/app/src/main/kotlin/br/com/segment/leos_music_shop_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.segment.leos_music_shop_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/mixins/home_mixin.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:leos_music_shop_flutter/app/modules/home/home_screen.dart';
+
+mixin HomeScreenMixin<T extends ConsumerStatefulWidget> on ConsumerState<HomeScreen> {
+
+}
