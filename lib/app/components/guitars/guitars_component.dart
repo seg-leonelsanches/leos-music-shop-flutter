e7040631e91a1e7cfd36881e5cf2e5453b67ab16
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-12 15:49:32 -0700

    Adding Shimmer.

--- lib/app/components/guitars/guitars_component.dart
+++ lib/app/components/guitars/guitars_component.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class GuitarsComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        Text(
-          'Guitars',
-        )
-      ],
+    return Shimmer.fromColors(
+      baseColor: const Color.fromARGB(0, 90, 90, 90),
+      highlightColor: const Color.fromARGB(0, 200, 200, 200),
+      child: Column(
+        children: const <Widget>[
+          Text(
+            'Guitars',
+          )
+        ],
+      ),
     );
   }
 }
